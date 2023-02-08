bece7b260e8a69cbe9122ef435288773a9e1cd62
fernandojosecc <fernandojosecc@hotmail.com>
2023-02-08 10:41:30 -0800

    Haciendo que mi canvas sea responsive

--- game.js
+++ game.js
@@ -2,26 +2,17 @@ const canvas = document.querySelector('#game'); // Select the HTML element with 
 const game = canvas.getContext('2d'); // Get the 2D rendering context for the canvas and store it in the constant variable 'game'.
 
 // Add a 'load' event listener to the window object, which calls the 'startGame' function when the window is finished loading.
-window.addEventListener('load',startGame);
+window.addEventListener('load',setCanvasSize);
+window.addEventListener('resize',setCanvasSize);
+
+let canvasSize;
+     
+let elementsSize;// = canvasSize / 10;
 
 // Define the 'startGame' function.
 function startGame() {
-    let canvasSize;
 
-     // Determine the size of the canvas based on the dimensions of the window.
-    if (window.innerHeight > window.innerWidth) {
-        canvasSize = window.innerWidth * 0.8;
-    }else{
-        canvasSize = window.innerHeight * 0.8;
-    }
 
-      // Set the width and height of the canvas to the calculated 'canvasSize'.
-    canvas.setAttribute('width',canvasSize)
-    canvas.setAttribute('height', canvasSize)
-
-     // Calculate the size of the elements to be drawn on the canvas.
-    const elementsSize = canvasSize / 10;
-    
      // Log the values of 'canvasSize' and 'elementsSize' to the console for debugging purposes.
     console.log({canvasSize, elementsSize});
 
@@ -34,19 +25,23 @@ function startGame() {
     for(let i=1;i<=10;i++){
         game.fillText(emojis['X'],elementsSize*i,elementsSize);
     }
-    
-    
 
-    // window.innerHeight
-    // window.innerWidth
 
-    // game.fillRect(0,0,100,100);
-    // game.clearRect(0,0,50,50);
-    // game.clearRect(0,0,50,50);
+}
 
-    // game.font = '25px Verdana';
-    // game.fillStyle = 'purple';
-    // game.textAlign = 'left';
-    // game.fillText('Platzi',100,100)
+function setCanvasSize(){
+    // Determine the size of the canvas based on the dimensions of the window.
+if (window.innerHeight > window.innerWidth) {
+    canvasSize = window.innerWidth * 0.8;
+}else{
+    canvasSize = window.innerHeight * 0.8;
+}
 
+  // Set the width and height of the canvas to the calculated 'canvasSize'.
+canvas.setAttribute('width',canvasSize)
+canvas.setAttribute('height', canvasSize)
+
+// Calculate the size of the elements to be drawn on the canvas.
+elementsSize = canvasSize/10;
+startGame();
 }
