160388410b29f26238a0c14327a5d4351f347f07
fernandojosecc <fernandojosecc@hotmail.com>
2023-02-25 15:21:23 -0800

    Refactor del mapa del juego

--- game.js
+++ game.js
@@ -37,19 +37,26 @@ function startGame() {
     game.font = elementsSize + 'px Verdana';
     game.textAlign = 'end';
 
-    const map= maps[2];
+    const map= maps[0];
     const mapRows = map.trim().split('\n');
     const mapRowCols = mapRows.map(row=>row.trim().split(''));
     console.log({map,mapRows,mapRowCols});
 
+    mapRowCols.forEach((row, rowI ) => {
+        row.forEach((col,colI) =>{
+            const emoji = emojis[col];
+            const posX = elementsSize *(colI+1);
+            const posY = elementsSize *(rowI+1);
+            game.fillText(emoji,posX,posY);
+        });
+    });
+
        // Loop 10 times and draw a text element on the canvas for each iteration.
-    for(let row=1;row<=10;row++){
-        for(let column = 1;column<=10;column++){
-            game.fillText(emojis[mapRowCols[row-1][column-1]],elementsSize*column,elementsSize*row);
-        }
+    // for(let row=1;row<=10;row++){
+    //     for(let column = 1;column<=10;column++){
+    //         game.fillText(emojis[mapRowCols[row-1][column-1]],elementsSize*column,elementsSize*row);
+    //     }
         
-    }
-
-
+    // }
 }
 
