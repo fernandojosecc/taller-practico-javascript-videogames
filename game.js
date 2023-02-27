932d98883f0f0e665e99b16c56f330f7421ebab8
fernandojosecc <fernandojosecc@hotmail.com>
2023-02-26 17:14:40 -0800

    Limpieza de movimientos en el canvas

--- game.js
+++ game.js
@@ -50,6 +50,8 @@ function startGame() {
     const mapRowCols = mapRows.map(row=>row.trim().split(''));
     console.log({map,mapRows,mapRowCols});
 
+    game.clearRect(0,0,canvasSize,canvasSize);
+
     mapRowCols.forEach((row, rowI ) => {
         row.forEach((col,colI) =>{
             const emoji = emojis[col];
@@ -57,15 +59,18 @@ function startGame() {
             const posY = elementsSize *(rowI+1);
 
             if(col=='O'){
-                playerPosition.x = posX;
-                playerPosition.y = posY;
-
-                console.log(playerPosition);
+                if(!playerPosition.x && !playerPosition.y){
+                    playerPosition.x = posX;
+                    playerPosition.y = posY;
+                    console.log({playerPosition});
+                }
             }
 
             game.fillText(emoji,posX,posY);
         });
     });
+
+    movePlayer();
 }
 
 function movePlayer(){
@@ -87,16 +92,22 @@ function moveByKeys(event){
 function moveUp(){
     console.log('Me quiero mover hacia arriba');
     playerPosition.y -=elementsSize;
-    movePlayer();
+    startGame();
 }
 function moveLeft(){
     console.log('Me quiero mover hacia izquierda');
+    playerPosition.x -=elementsSize;
+    startGame();
 }
 function moveRight(){
     console.log('Me quiero mover hacia derecha');
+    playerPosition.x +=elementsSize;
+    startGame();
 }
 function moveDown(){
     console.log('Me quiero mover hacia abajo');
+    playerPosition.y +=elementsSize;
+    startGame();
 }
 
 
